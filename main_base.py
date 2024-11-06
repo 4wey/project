b3e1eb3330643d6bc1325268179e189247ad43e0
4wey <stas101106@gmail.com>
2024-11-06 18:21:21 +0300

    Add files via upload

---
+++ main_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from project_func.cli import welcome
+
+
+def main():
+    print('Первая попытка запустить проект!')
+    welcome()
+
+
+if __name__ == '__main__':
+    main()
